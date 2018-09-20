3f0caba87a2738cc42c6aad920d30741e68fb8da
Mark Vincze <mrk.vincze@gmail.com>
2018-09-20 15:35:04 +0200

    Revert some changes
    Signed-off-by: Mark Vincze <mrk.vincze@gmail.com>

--- ci/do_ci.sh
+++ ci/do_ci.sh
@@ -116,8 +116,7 @@ elif [[ "$1" == "bazel.dev" ]]; then
     "${ENVOY_CI_DIR}"/bazel-bin/source/exe/envoy-static \
     "${ENVOY_DELIVERY_DIR}"/envoy-fastbuild
   echo "Building and testing..."
-  echo "Skipping tests"
-  # bazel --batch test ${BAZEL_TEST_OPTIONS} -c fastbuild //test/...
+  bazel --batch test ${BAZEL_TEST_OPTIONS} -c fastbuild //test/...
   exit 0
 elif [[ "$1" == "bazel.ipv6_tests" ]]; then
   # This is around until Circle supports IPv6. We try to run a limited set of IPv6 tests as fast
